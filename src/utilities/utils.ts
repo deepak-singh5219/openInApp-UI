d8edfc3a36f15c37987f8c94e80bbf122616754c
Deepak <singhdeepak5219@gmail.com>
2023-09-02 16:48:16 +0530

    debug test

--- src/utilities/utils.ts
+++ src/utilities/utils.ts
@@ -2,5 +2,6 @@ import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
+    console.log("Hello"); // debug check
     return twMerge(clsx(inputs));
 }
