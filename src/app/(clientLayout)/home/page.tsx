c0b810d125cf33430fed83705f230fa3e98edb01
Deepak Singh <singhdeepak5219@gmail.com>
2023-09-04 00:49:51 +0530

    added profile form

--- src/app/(clientLayout)/home/page.tsx
+++ src/app/(clientLayout)/home/page.tsx
@@ -1,24 +1,26 @@
 "use client";
 import { SidebarContext } from "../../../context/SidebarContext";
-import { FC } from "react";
+import { FC, useState } from "react";
 import React, { useContext } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import Capsules from "../../../components/Capsules";
 import { Text } from "../../../components/UI";
-import NavBar from "@/components/NavBar";
 import AddIcon from "@mui/icons-material/Add";
-import BarChart from "@/components/Charts/BarChart"
+import BarChart from "@/components/Charts/BarChart";
 import MobileChart from "../../../components/Charts/MobileChart";
 import { useDataApi } from "../../../context/ApiContext";
-
-
+import FormComponent from "@/components/FormComponent";
 
 interface pageProps {}
 
 const Home: FC<pageProps> = ({}) => {
   const { isOpen, toggleSidebar } = useContext(SidebarContext);
   const { data, loading, error } = useDataApi();
-  
+  const [addPopup, setAddPopup] = useState<Boolean>(false);
+  const handleClick = () => {
+    setAddPopup(true);
+  };
+
   return (
     <>
       <MenuIcon
@@ -28,7 +30,6 @@ const Home: FC<pageProps> = ({}) => {
         } absolute md:hidden cursor-pointer left-4 top-4`}
       />
       {/* <NavBar/> */}
-
       <div className="flex w-full flex-wrap items-center md:justify-between">
         <Capsules />
         <Capsules />
@@ -37,22 +38,29 @@ const Home: FC<pageProps> = ({}) => {
       </div>
       <div className="w-full h-[90%] border-gray-200 border-2 rounded-lg my-4 border-gray-200 shadow-lg ">
         <div className="w-full h-full hidden md:flex">
-        <BarChart data = {data}/>
+          <BarChart data={data} />
         </div>
         <div className="md:hidden w-full h-full">
-        <MobileChart data = {data}/>
+          <MobileChart data={data} />
         </div>
       </div>
-      <div className="w-full flex items-center justify-between h-72 my-4">
-        <div className="rounded-xl  border-2 shadow-lg border-gray-200 w-[45%] h-full"></div>
-        <div className="rounded-xl border-2 shadow-lg border-gray-200 w-[45%] h-full">
-          <div className="flex items-center justify-between w-full h-full bg-[#fe2]">
-            <div className="w-20 h-20 rounded-full p-4 bg-dark items-center">
-              <AddIcon fontSize="large" />
+      <div className="w-full flex items-center gap-x-5 h-72 my-4">
+        <div className="rounded-xl  border-2 shadow-lg border-gray-200 w-[50%] h-full"></div>
+        <div className="rounded-xl border-2 shadow-lg border-gray-200 w-[50%] h-full">
+          <div className="flex items-center flex-col justify-center w-full h-full">
+            <div className="w-20 h-20 rounded-full p-4 bg-[#F2F2F2] text-[#999CA0] flex items-center justify-center">
+              <AddIcon fontSize="large" onClick={handleClick} />
             </div>
-            <Text variant="titleSm">See All</Text>
+            <Text variant="productTitle" className="mt-3 ">
+              Add Profile
+            </Text>
           </div>
         </div>
+        {addPopup ? (
+          <div className="bg-[#323232e6] fixed top-0 left-0 w-full h-screen z-50">
+            <FormComponent setAddPopup={setAddPopup} />
+          </div>
+        ) : null}
       </div>
     </>
   );
---
+++ src/components/Contact.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { Button, Input, Text } from "./UI";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+type Props = {
+  setState: (state: String) => void;
+};
+const schema = z.object({
+  instagram: z.string().min(1, { message: "enter a valid link" }),
+  youtube: z.string().min(10, { message: "enter a valid link" }),
+});
+export type IForm = z.infer<typeof schema>;
+
+function Contact({ setState }: Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({ resolver: zodResolver(schema) });
+
+  const onSubmitReady = async (data: IForm) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <form className="space-y-6" onSubmit={handleSubmit(onSubmitReady)}>
+        <div>
+          <Text variant="titleSm" className="inline-block max-w-max mb-2">
+            Instagram Link (optional)
+          </Text>
+          <Input
+            variant="image"
+            placeholder="www.instagram.com/username"
+            className="block w-full rounded-md  py-1.5 text-primary bg-cardSecondary placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-buttonSecondary sm:text-sm sm:leading-6 border border-[#E5E5E5]"
+            register={register("instagram")}
+          />
+          {errors.instagram?.message && (
+            <Text variant="error" className="ml-2 mt-2">
+              {errors.instagram?.message}
+            </Text>
+          )}
+        </div>
+        <div>
+          <Text variant="titleSm" className="inline-block max-w-max mb-2">
+            Youtube Link (optional)
+          </Text>
+          <Input
+            variant="image"
+            placeholder="www.youtube.com/username"
+            className="block w-full rounded-md  py-1.5 text-primary bg-cardSecondary shadow-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-buttonSecondary sm:text-sm sm:leading-6"
+            register={register("youtube")}
+          />
+          {errors.youtube?.message && (
+            <Text variant="error" className="ml-2 mt-2">
+              {errors.youtube?.message}
+            </Text>
+          )}
+        </div>
+        <div className="mt-20 flex justify-end gap-x-4">
+          <Button
+            variant="outline"
+            title="Back"
+            className="text-black"
+            onClick={() => setState("basic")}
+          />
+          <Button
+            type="submit"
+            variant="primary"
+            title="Done"
+            className="text-white"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
