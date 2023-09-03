ac6ef4caff1f2be3d1140cf0b6689a7145467c2f
Deepak Singh <singhdeepak5219@gmail.com>
2023-09-03 21:56:16 +0530

    form UI

--- src/app/(clientLayout)/home/page.tsx
+++ src/app/(clientLayout)/home/page.tsx
@@ -1,14 +1,13 @@
 "use client";
-import { SidebarContext } from "../../../context/SidebarContext"
+import { SidebarContext } from "../../../context/SidebarContext";
 import { FC } from "react";
-import React, { useContext } from 'react';
-import MenuIcon from '@mui/icons-material/Menu';
+import React, { useContext } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 import Capsules from "../../../components/Capsules";
 import { Text } from "../../../components/UI";
 import NavBar from "@/components/NavBar";
-import BarChart from "@/components/Charts/BarChart"
-
-
+import BarChart from "@/components/Charts/BarChart";
+import AddIcon from "@mui/icons-material/Add";
 
 interface pageProps {}
 
@@ -16,8 +15,13 @@ const Home: FC<pageProps> = ({}) => {
   const { isOpen, toggleSidebar } = useContext(SidebarContext);
   return (
     <>
-    <MenuIcon onClick={toggleSidebar} className={`${!isOpen?"text-gray-200":"text-gray-800"} absolute md:hidden cursor-pointer left-4 top-4`}/>
-     {/* <NavBar/> */}
+      <MenuIcon
+        onClick={toggleSidebar}
+        className={`${
+          !isOpen ? "text-gray-200" : "text-gray-800"
+        } absolute md:hidden cursor-pointer left-4 top-4`}
+      />
+      {/* <NavBar/> */}
 
       <div className="flex w-full flex-wrap items-center justify-between">
         <Capsules />
@@ -27,14 +31,19 @@ const Home: FC<pageProps> = ({}) => {
       </div>
 
       <div className="w-full h-72 border-gray-200 border-2 rounded-lg my-4 border-gray-200 shadow-lg ">
-        <BarChart/>
+        <BarChart />
       </div>
       <div className="w-full flex items-center justify-between h-72 my-4">
         <div className="rounded-xl  border-2 shadow-lg border-gray-200 w-[45%] h-full"></div>
-        <div className="rounded-xl border-2 shadow-lg border-gray-200 w-[45%] h-full"></div>
+        <div className="rounded-xl border-2 shadow-lg border-gray-200 w-[45%] h-full">
+          <div className="flex items-center justify-between w-full h-full bg-[#fe2]">
+            <div className="w-20 h-20 rounded-full p-4 bg-dark items-center">
+              <AddIcon fontSize="large" />
+            </div>
+            <Text variant="titleSm">See All</Text>
+          </div>
+        </div>
       </div>
-
-
     </>
   );
 };
---
+++ src/components/Form.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Form({}: Props) {
+  return (
+    <div>Form</div>
+  )
+}
+
+export default Form
