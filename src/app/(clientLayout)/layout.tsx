76d94da464277c2b5753a92e13e8a4e18fc81001
Deepak Singh <singhdeepak5219@gmail.com>
2023-09-03 19:41:53 +0530

    API created for data

--- src/app/(clientLayout)/layout.tsx
+++ src/app/(clientLayout)/layout.tsx
@@ -1,10 +1,7 @@
-
 import NavBar from "@/components/NavBar";
 import SideBar from "@/components/SideBar";
 import SidebarProvider from "@/context/SidebarContext";
-import DataApiProvider from "@/context/ApiContext"
-
-
+import DataApiProvider from "@/context/ApiContext";
 export const metadata = {
   title: "openinappui",
   description: "",
@@ -17,18 +14,20 @@ export default function RootLayout({
 }) {
   return (
     <>
-    <DataApiProvider>
-    <SidebarProvider>
-    <div className="h-screen px-10 fixed w-screen md:m-6 md:p-2 flex flex-row justify-start">
-        <SideBar />
-        <div className="flex flex-col flex-1 w-full mt-4 md:mt-0 mx-2 md:px-12 px-4 ">
-        <NavBar/>
-         
-        <div className="bg-primary overflow-y-auto mt-6 flex-2 px-4 mb-6 text-primary">{children}</div>
-        </div>
-      </div>
-    </SidebarProvider>
-    </DataApiProvider>
+      <DataApiProvider>
+        <SidebarProvider>
+          <div className="h-screen px-10 fixed w-screen md:m-6 md:p-2 flex flex-row justify-start">
+            <SideBar />
+            <div className="flex flex-col flex-1 w-full mt-4 md:mt-0 md:px-8 px-2 ">
+              <NavBar />
+
+              <div className="bg-primary overflow-y-auto mt-6 flex-2  mb-6 text-primary">
+                {children}
+              </div>
+            </div>
+          </div>
+        </SidebarProvider>
+      </DataApiProvider>
     </>
   );
 }
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,5 +1,4 @@
 import NextAuth from 'next-auth';
-
 declare module 'next-auth' {
     interface Session {
         user: {
