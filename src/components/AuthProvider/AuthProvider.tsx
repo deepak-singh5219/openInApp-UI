94acf23a3f65cd701351505eaf776042d41bf463
Deepak Singh <singhdeepak5219@gmail.com>
2023-09-04 13:01:55 +0530

    debug test2

--- src/components/AuthProvider/AuthProvider.tsx
+++ src/components/AuthProvider/AuthProvider.tsx
@@ -1,13 +1,16 @@
 "use client";
 import React from "react";
 import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
+
 
 type Props = {
   children: React.ReactNode;
+  session?: Session;
 };
 
-const AuthProvider = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>;
+const AuthProvider = ({ children, session }: Props) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 
 export default AuthProvider;
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,12 +2,14 @@
 import { FC, ReactNode } from "react";
 import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
+
 interface ProvidersProps {
   children: ReactNode;
   session?: Session;
 }
 
 const Providers: FC<ProvidersProps> = ({ children, session }) => {
+  console.log(session)
   return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 
